cec3832886ab3b5b836105b046826f4bbfac7e0a
Jason McMillan <jason.mcmillan.777@gmail.com>
2022-10-29 15:55:25 -0600

    more refactor

--- js/script.js
+++ js/script.js
@@ -63,3 +63,11 @@ function contact() {
     contactElement.style.display = "block";
 }
 
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "nav-container") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-container";
+    }
+}
