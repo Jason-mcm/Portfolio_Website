0aed70c9d82f1257366e77880f4fb1586b9c7560
Jason McMillan <jason.mcmillan.777@gmail.com>
2022-10-29 11:59:51 -0600

    refactor

---
+++ js/script.js
@@ -0,0 +1,65 @@
+function home() {
+    let homeElement = document.getElementById("home");
+    let resumeElement = document.getElementById("resume");
+    // let projectsElement = document.getElementById("projects");
+    let aboutElement = document.getElementById("about");
+    let contactElement = document.getElementById("contact");
+    homeElement.style.display = "block";
+    resumeElement.style.display = "none";
+    // projectsElement.style.display = "none";
+    aboutElement.style.display = "none";
+    contactElement.style.display = "none";
+}
+
+function resume() {
+    let homeElement = document.getElementById("home");
+    let resumeElement = document.getElementById("resume");
+    // let projectsElement = document.getElementById("projects");
+    let aboutElement = document.getElementById("about");
+    let contactElement = document.getElementById("contact");
+    homeElement.style.display = "none";
+    resumeElement.style.display = "block";
+    // projectsElement.style.display = "none";
+    aboutElement.style.display = "none";
+    contactElement.style.display = "none";
+}
+
+// function projects() {
+//     let homeElement = document.getElementById("home");
+//     let resumeElement = document.getElementById("resume");
+//     // let projectsElement = document.getElementById("projects");
+//     let aboutElement = document.getElementById("about");
+//     let contactElement = document.getElementById("contact");
+//     homeElement.style.display = "none";
+//     resumeElement.style.display = "none";
+//     // projectsElement.style.display = "block";
+//     aboutElement.style.display = "none";
+//     contactElement.style.display = "none";
+// }
+
+function about() {
+    let homeElement = document.getElementById("home");
+    let resumeElement = document.getElementById("resume");
+    // let projectsElement = document.getElementById("projects");
+    let aboutElement = document.getElementById("about");
+    let contactElement = document.getElementById("contact");
+    homeElement.style.display = "none";
+    resumeElement.style.display = "none";
+    // projectsElement.style.display = "none";
+    aboutElement.style.display = "block";
+    contactElement.style.display = "none";
+}
+
+function contact() {
+    let homeElement = document.getElementById("home");
+    let resumeElement = document.getElementById("resume");
+    // let projectsElement = document.getElementById("projects");
+    let aboutElement = document.getElementById("about");
+    let contactElement = document.getElementById("contact");
+    homeElement.style.display = "none";
+    resumeElement.style.display = "none";
+    // projectsElement.style.display = "none";
+    aboutElement.style.display = "none";
+    contactElement.style.display = "block";
+}
+
